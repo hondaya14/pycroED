470f8780d907758ea0ed23dd238d487e9dbc5419
Yasuhisa Honda <nqvno14@gmail.com>
2021-04-13 22:40:48 +0900

    Gaussianユーティリティのapiコールを実装

--- lib/gaussian_util.py
+++ lib/gaussian_util.py
@@ -2,10 +2,23 @@ import subprocess
 
 
 def form_chk(file_name):
-    print('formchk file_name' + '.chk' + file_name + 'fchk')
-    subprocess.call(['formchk', file_name + '.chk', file_name + 'fchk'])
+    form_check_command = ['formchk', file_name + '.chk', file_name + '.fchk']
+    print(' '.join(form_check_command))
+    subprocess.call(form_check_command)
     print('')
 
 
-def generate_cube(input_file):
-    pass
+def generate_cube(file_name):
+    # generate cube file (3D: cube_file_size^3)
+    cube_file_size = 100
+    generate_density_cube_command = \
+        ['cubegen', '0', 'density', file_name+'.fchk', file_name+'_density.cube', str(cube_file_size)]
+    print(' '.join(generate_density_cube_command))
+    subprocess.call(generate_density_cube_command)
+    print('')
+
+    generate_potential_cube_command = \
+        ['cubegen', '0', 'potential', file_name+'.fchk', file_name+'_potential.cube', str(cube_file_size)]
+    print(' '.join(generate_potential_cube_command))
+    subprocess.call(generate_potential_cube_command)
+    print('')
--- main.py
+++ main.py
@@ -9,21 +9,34 @@ def main():
     gjf_file_name = sys.argv[1]
     file_name = gjf_file_name.split('.')[-2]
 
-    throw_gaussian_job(gjf_file_name)
+    # throw_gaussian_job(gjf_file_name)
 
     # form chk
-    form_chk(file_name)
+    # form_chk(file_name)
 
     # cubegen
+    # generate_cube(file_name)
+    density_file_name = file_name + '_density'
+    potential_file_name = file_name + '_potential'
 
     # cubepreprocess
-    # arra2mrc
+    # cube_preprocessor(density_file_name)
+    # cube_preprocessor(potential_file_name)
+
+    # array2mrc
+    # array2mrc(density_file_name)
+    # array2mrc(potential_file_name)
+
     # mrcCrystalCreate
+    crystal_create(density_file_name)
+    crystal_create(potential_file_name)
+
     # padding
+    
     # mrcImageFFT
     # mrcFFTExpression
     # mrc2ascii
-    # pickuphkl
+    # pickup hkl
     # R factor calculation
 
 
